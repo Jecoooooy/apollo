35fc20fa94e0e3bbf8351f2c5889a2f10d12d236
Jerico <bencitojerico@gmail.com>
2024-11-10 11:37:23 +0800

    feat: fix layout

--- client/pages/launches/index.vue
+++ client/pages/launches/index.vue
@@ -1,36 +1,54 @@
 <template>
-	<section>
+	<v-container>
 		<h1>SpaceX Launches</h1>
-		<v-container>
-			<v-row>
-				<v-col v-for="(launch, index) in launches" :key="index" cols="3">
-					<v-card elevation="5">
-						<v-card-title tag="h4" class="bg-primary">
-							{{ launch.mission_name }}
-						</v-card-title>
-						<v-card-text>
-							<p>Launch Date: {{ formatDate(launch.launch_date_local) }}</p>
-							<p v-if="launch.launch_site">Launch Site:{{ launch.launch_site.site_name }}</p>
-							<p>rocket : {{ launch.rocket.rocket_name }}</p>
-							<br />
-							<p style="overflow-y: auto; height: 70px">Details : {{ launch.details }}</p>
-						</v-card-text>
-						<v-divider />
-						<v-card-actions>
-							<v-spacer />
-							<v-btn color="primary" variant="elevated" text="view" />
-						</v-card-actions>
-					</v-card>
-				</v-col>
-			</v-row>
-		</v-container>
-	</section>
+		<v-row>
+			<v-col cols="12" sm="12" md="6" lg="6" xl="6" xxl="6">
+				<v-text-field label="search" prepend-inner-icon="mdi-magnify" />
+			</v-col>
+			<v-col cols="12" sm="12" md="6" lg="6" xl="6" xxl="6" class="d-flex justify-end">
+				<v-pagination v-model="currentPage" class="mr-n1" :length="totalPages" :total-visible="5" />
+			</v-col>
+			<v-col
+				v-for="(launch, index) in paginatedItems"
+				:key="index"
+				cols="6"
+				sm="6"
+				md="4"
+				lg="3"
+				xl="3"
+				xxl="2"
+			>
+				<v-card elevation="5">
+					<v-card-title tag="h4" class="bg-primary">
+						{{ launch.mission_name }}
+					</v-card-title>
+					<v-card-text>
+						<p>Launch Date: {{ formatDate(launch.launch_date_local) }}</p>
+						<p>
+							Launch Site:{{
+								launch.launch_site ? launch.launch_site.site_name : launch.launch_site
+							}}
+						</p>
+						<p>rocket : {{ launch.rocket.rocket_name }}</p>
+						<br />
+						<p>Details :</p>
+						<p style="overflow-y: auto; height: 70px">{{ launch.details }}</p>
+					</v-card-text>
+					<v-divider />
+					<v-card-actions>
+						<v-spacer />
+						<v-btn color="primary" variant="elevated" text="view" />
+					</v-card-actions>
+				</v-card>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script lang="ts" setup>
 const query = gql`
 	query {
-		launches(limit: 1) {
+		launches {
 			id
 			mission_id
 			mission_name
@@ -89,18 +107,29 @@ function formatDate(date: Date): string {
 		'December',
 	]
 	const currentDate = new Date(date)
-	let hours: number = currentDate.getHours()
-	let minutes: number = currentDate.getMinutes()
+	// let hours: number = currentDate.getHours()
+	// let minutes: number = currentDate.getMinutes()
 	const month: string = monthNames[currentDate.getMonth()]
 	let day: number = currentDate.getDate()
 	const year: number = currentDate.getFullYear()
 
-	hours = hours < 10 ? parseInt('0' + hours, 10) : hours
-	minutes = minutes < 10 ? parseInt('0' + minutes, 10) : minutes
+	// hours = hours < 10 ? parseInt('0' + hours, 10) : hours
+	// minutes = minutes < 10 ? parseInt('0' + minutes, 10) : minutes
 	day = day < 10 ? parseInt('0' + day, 10) : day
 
-	return `${month} ${day}, ${year} - ${hours}:${minutes}`
+	// return `${month} ${day}, ${year} - ${hours}:${minutes}`
+	return `${month} ${day}, ${year}`
 }
+
+const currentPage = ref(1)
+const itemsPerPage = 4
+const totalPages = computed(() => Math.ceil(launches.value.length / itemsPerPage))
+
+const paginatedItems = computed(() => {
+	const start = (currentPage.value - 1) * itemsPerPage
+	const end = start + itemsPerPage
+	return launches.value.slice(start, end)
+})
 </script>
 
 <style scoped>
