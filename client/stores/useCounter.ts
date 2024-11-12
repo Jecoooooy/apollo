38725979af58e20d8eb50f8e1e065dd04f6fb279
Jerico <bencitojerico@gmail.com>
2024-11-12 08:07:26 +0800

    feat: moving the launches to the global store

--- client/stores/useCounter.ts
+++ client/stores/useCounter.ts
@@ -1,7 +1,12 @@
+import { GET_LAUNCHES_QUERY } from '@/graphql/launchesQuery'
+import type { Launch } from '@/graphql/launchesQuery'
 export const useCounter = defineStore('counter', {
 	state: () => ({
 		count: 0,
 		drawer: false,
+		pageTitle: 'Home',
+		launches: [] as Launch[],
+		loading: false,
 	}),
 	getters: {
 		doubleCount: (state) => state.count * 2,
@@ -9,6 +14,23 @@ export const useCounter = defineStore('counter', {
 	actions: {
 		increment() {
 			this.count++
+		},
+
+		getLaunchesData() {
+			this.loading = true
+
+			const { result, error, loading: gqlLoading } = useQuery(GET_LAUNCHES_QUERY)
+
+			watch(gqlLoading, (newLoading) => {
+				if (!newLoading && result.value) {
+					this.launches = result.value.launches
+					this.loading = gqlLoading.value
+				}
+			})
+
+			if (error.value) {
+				console.error('Error fetching launches:', error.value)
+			}
 		},
 	},
 })
