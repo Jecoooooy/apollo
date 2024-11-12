31e45909c2d553fbfc9f92b6ef96bcbdf3c63812
Jerico <bencitojerico@gmail.com>
2024-11-12 08:50:34 +0800

    style: fix indention and spaces

--- client/pages/launches/index.vue
+++ client/pages/launches/index.vue
@@ -1,95 +1,80 @@
 <template>
-	<v-container>
-		<h1>SpaceX Launches</h1>
-		<v-row>
-			<v-col cols="12" sm="12" md="6" lg="6" xl="6" xxl="6">
+	<v-container class="position-relative h-100">
+		<v-row class="text-secondary py-4">
+			<v-col cols="12" sm="6" md="6" lg="4" xl="3" xxl="3">
 				<v-text-field label="search" prepend-inner-icon="mdi-magnify" />
 			</v-col>
-			<v-col cols="12" sm="12" md="6" lg="6" xl="6" xxl="6" class="d-flex justify-end">
-				<v-pagination v-model="currentPage" class="mr-n1" :length="totalPages" :total-visible="5" />
+			<v-col cols="12" sm="6" md="6" lg="4" xl="3" xxl="3">
+				<c-date-picker label="Launch Date" @date="launchDate" @input="launchDateSelected" />
 			</v-col>
-			<v-col
-				v-for="(launch, index) in paginatedItems"
-				:key="index"
-				cols="6"
-				sm="6"
-				md="4"
-				lg="3"
-				xl="3"
-				xxl="2"
-			>
-				<v-card elevation="5">
-					<v-card-title tag="h4" class="bg-primary">
-						{{ launch.mission_name }}
-					</v-card-title>
-					<v-card-text>
-						<p>Launch Date: {{ formatDate(launch.launch_date_local) }}</p>
-						<p>
-							Launch Site:{{
-								launch.launch_site ? launch.launch_site.site_name : launch.launch_site
-							}}
-						</p>
-						<p>rocket : {{ launch.rocket.rocket_name }}</p>
-						<br />
-						<p>Details :</p>
-						<p style="overflow-y: auto; height: 70px">{{ launch.details }}</p>
-					</v-card-text>
-					<v-divider />
-					<v-card-actions>
-						<v-spacer />
-						<v-btn color="primary" variant="elevated" text="view" />
-					</v-card-actions>
-				</v-card>
+			<v-col cols="12" sm="12" md="12" lg="4" xl="6" xxl="6" class="d-flex justify-end">
+				<v-pagination
+					v-model="currentPage"
+					class="mr-n1"
+					:length="totalPages"
+					:total-visible="4"
+					@update:modelValue="changePagination()"
+				/>
 			</v-col>
 		</v-row>
+		<article class="position-relative h-100">
+			<TransitionGroup name="slide-card" tag="div" class="v-row align-stretch" mode="out-in">
+				<v-col
+					v-for="(launch, index) in paginatedItems"
+					v-show="hide[index]"
+					:key="index"
+					cols="6"
+					sm="6"
+					md="4"
+					lg="3"
+					xl="3"
+					xxl="2"
+				>
+					<v-card elevation="10" color="transparent" class="launch-card rounded-lg">
+						<v-card-title tag="h4" class="text-secondary bg-white">
+							{{ launch.mission_name }}
+						</v-card-title>
+						<v-card-text class="text-secondary">
+							<p>Launch Date: {{ formatDate(launch.launch_date_local) }}</p>
+							<p>
+								Launch Site:{{
+									launch.launch_site ? launch.launch_site.site_name : launch.launch_site
+								}}
+							</p>
+							<p>rocket : {{ launch.rocket.rocket_name }}</p>
+							<br />
+							<p class="text-body-2">Details :</p>
+							<p class="text-caption" style="overflow-y: scroll; height: 40px">
+								{{ launch.details }}
+							</p>
+						</v-card-text>
+						<v-divider />
+						<v-card-actions>
+							<v-btn icon color="secondary" density="compact">
+								<v-icon>mdi-star-outline</v-icon>
+								<v-tooltip activator="parent" location="top">Tooltip</v-tooltip>
+							</v-btn>
+							<v-spacer />
+							<v-btn color="secondary" variant="elevated" text="view Launch" />
+						</v-card-actions>
+					</v-card>
+				</v-col>
+			</TransitionGroup>
+			<transition name="slide-down" mode="out-in">
+				<div
+					v-if="!store.launches.length"
+					class="w-100 position-absolute d-flex flex-column justify-center align-center h-100 loader-container"
+				>
+					<div class="loader" />
+					<p class="text-white"><strong>Loading...</strong></p>
+				</div>
+			</transition>
+		</article>
 	</v-container>
 </template>
 
 <script lang="ts" setup>
-const query = gql`
-	query {
-		launches {
-			id
-			mission_id
-			mission_name
-			launch_date_local
-			launch_site {
-				site_id
-				site_name
-				site_name_long
-			}
-			rocket {
-				rocket_name
-				rocket_type
-				rocket {
-					name
-					type
-				}
-			}
-			details
-		}
-	}
-`
-
-const { data } = useAsyncQuery<{
-	launches: {
-		id: String
-		mission_id: String
-		mission_name: [String]
-		launch_date_local: Date
-		launch_site: {
-			site_id: String
-			site_name: String | null
-			site_name_long: String | null
-		} | null
-		rocket: {
-			rocket_name: String
-			rocket_type: String
-		}
-		details: String | null
-	}[]
-}>(await query)
-const launches = computed(() => data.value?.launches ?? [])
+const store = useCounter()
 
 function formatDate(date: Date): string {
 	const monthNames: string[] = [
@@ -121,17 +106,65 @@ function formatDate(date: Date): string {
 	return `${month} ${day}, ${year}`
 }
 
+// pagination
 const currentPage = ref(1)
-const itemsPerPage = 4
-const totalPages = computed(() => Math.ceil(launches.value.length / itemsPerPage))
+const itemsPerPage = 8
+const totalPages = computed(() => Math.ceil(store.launches.length / itemsPerPage))
 
 const paginatedItems = computed(() => {
 	const start = (currentPage.value - 1) * itemsPerPage
 	const end = start + itemsPerPage
-	return launches.value.slice(start, end)
+	return store.launches.slice(start, end)
+})
+const launchDate = ref<string>('')
+const launchDateSelected = (date: string) => {
+	launchDate.value = date
+	console.log('Selected Launch Date:', launchDate.value)
+}
+
+// launch card transistions
+const hide = ref<boolean[]>([])
+
+function changePagination() {
+	for (let index = 0; index < itemsPerPage; index++) {
+		hide.value[index] = false
+	}
+	let delay = 300
+	for (let index = 0; index < itemsPerPage; index++) {
+		setTimeout(() => {
+			hide.value[index] = true
+		}, delay)
+		delay += 100
+	}
+}
+
+watch(
+	() => store.loading,
+	() => {
+		if (store.launches) {
+			changePagination()
+		}
+	},
+)
+
+onMounted(async () => {
+	await store.getLaunchesData()
+	if (store.launches) {
+		changePagination()
+	}
 })
 </script>
 
 <style scoped>
-/* Add any styles you want here */
+.launch-card {
+	backdrop-filter: blur(12px);
+	background-image: linear-gradient(45deg, #fafafa65, #2121213f);
+	transition: all 0.2s ease;
+}
+
+.loader-container {
+	z-index: 1;
+	top: 0;
+	left: 0;
+}
 </style>
