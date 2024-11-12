e751d8dc173a36c43ae4d81ca830a28f9f26c6b9
Jerico <bencitojerico@gmail.com>
2024-11-12 08:52:24 +0800

    style: remove comment

--- client/components/cFooter.vue
+++ client/components/cFooter.vue
@@ -1,17 +1,9 @@
 <template>
-	<v-footer app class="px-4 py-2 bg-black text-body-2 text-center w-100">
+	<v-footer app class="px-4 py-2 bg-black text-body-2 text-center">
 		<h5 class="text-center w-100">
 			This project is powered by: Nuxt, ES2020, Vuetify, GraphQL, Pinia and Vue3 - ©
 			{{ new Date().getFullYear() }}
 		</h5>
 	</v-footer>
 </template>
-<script lang="ts" setup>
-// const icons = ref([
-//     'mdi-facebook',
-//     'mdi-twitter',
-//     'mdi-linkedin',
-//     'mdi-instagram',
-// ])
-// console.log(icons.value);
-</script>
+<script lang="ts" setup></script>
