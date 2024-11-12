119a4c87acc12bc9df3d43307a5915532c13b7f8
Jerico <bencitojerico@gmail.com>
2024-11-12 08:28:11 +0800

    feat: add page title in navbar

--- client/components/cAppBar.vue
+++ client/components/cAppBar.vue
@@ -1,9 +1,31 @@
 <template>
-	<v-app-bar>
-		<v-app-bar-nav-icon @click="store.drawer = !store.drawer" />
-		<v-app-bar-title>Nuxt - ES2020 - Vuetify - GraphQL - Pinia - Vue3</v-app-bar-title>
+	<v-app-bar app class="appbar-style">
+		<v-app-bar-nav-icon rounded @click="store.drawer = !store.drawer" />
+		<v-app-bar-title>SpaceX / {{ store.pageTitle }}</v-app-bar-title>
 	</v-app-bar>
 </template>
 <script lang="ts" setup>
 const store = useCounter()
+const route = useRoute()
+
+function dispalyTitle() {
+	console.log(route.name)
+
+	if (route.name === 'index') {
+		store.pageTitle = 'Home'
+	} else {
+		store.pageTitle = route.name ? String(route.name) : 'Unknown'
+		console.log(store.pageTitle)
+	}
+}
+
+// onMounted(() => {
+dispalyTitle()
+
+// })
 </script>
+<style scope>
+.appbar-style {
+	border-radius: 0 0 12px 12px !important;
+}
+</style>
