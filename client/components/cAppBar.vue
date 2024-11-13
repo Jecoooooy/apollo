eca9cb3fc3a3a5880cb090abe95dc63f0598da61
Jerico <bencitojerico@gmail.com>
2024-11-14 07:13:53 +0800

    feat: add navigation for desktop

--- client/components/cAppBar.vue
+++ client/components/cAppBar.vue
@@ -1,10 +1,27 @@
 <template>
-	<v-app-bar app class="appbar-style" scroll-behavior="fully-hide" style="transition: all 0.5s ease">
-		<v-app-bar-nav-icon v-if="store.windowWidth <= 1280" rounded @click="store.drawer = !store.drawer" />
+	<v-app-bar app class="appbar-style" color="transparent" scroll-behavior="fully-hide">
+		<v-app-bar-nav-icon
+			v-if="store.windowWidth <= 1280"
+			rounded
+			style="filter: invert(1)"
+			@click="store.drawer = !store.drawer"
+		/>
 		<div class="image-container pl-4 ma-auto">
 			<img src="../static/SpaceXLogo.png" aspect-ratio="16/9" />
 		</div>
-		<v-app-bar-title :class="' '">/ {{ store.pageTitle }}</v-app-bar-title>
+		<v-app-bar-title style="filter: invert(1)">/ {{ store.pageTitle }}</v-app-bar-title>
+		<template v-if="store.windowWidth >= 1280" #append>
+			<v-list-item
+				v-for="(link, index) in routes"
+				:key="index"
+				link
+				class="text-uppercase text-white rounded"
+				style="filter: invert(1)"
+				:title="link.name && typeof link.name === 'string' ? link.name : 'Home'"
+				:to="link.path"
+				@click="changeTitle(link.name as string)"
+			/>
+		</template>
 	</v-app-bar>
 </template>
 <script lang="ts" setup>
@@ -12,13 +29,21 @@ const store = useCounter()
 const route = useRoute()
 
 function dispalyTitle() {
-	// console.log(route.name)
-
 	if (route.name === 'index') {
 		store.pageTitle = 'Home'
 	} else {
 		store.pageTitle = route.name ? String(route.name) : 'Unknown'
-		// console.log(store.pageTitle)
+	}
+}
+
+const router = useRouter()
+const routes = router.options.routes
+
+function changeTitle(item: string) {
+	if (item === 'index') {
+		store.pageTitle = 'Home'
+	} else {
+		store.pageTitle = item
 	}
 }
 
@@ -30,6 +55,8 @@ dispalyTitle()
 <style scope>
 .appbar-style {
 	/* border-radius: 0 0 12px 12px !important; */
+	background-image: linear-gradient(-45deg, rgb(255 255 255 / 49.3%), rgb(0 0 0 / 24.7%)) !important;
+	backdrop-filter: blur(12px);
 
 	/* background-color: transparent!important; */
 }
