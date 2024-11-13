9cefa4d57e9b6ac262b37ccb5906d423b02353e4
Jerico <bencitojerico@gmail.com>
2024-11-14 07:30:23 +0800

    feat: add middleware that will redirect user to launch page if route is non existent

--- client/middleware/redirect.global.ts
+++ client/middleware/redirect.global.ts
@@ -1,8 +1,6 @@
 export default defineNuxtRouteMiddleware((to) => {
-	// Log the incoming route details
 	console.log('Navigating to:', to.fullPath)
 
-	// If the route doesn't match anything, redirect to the home page
 	if (!to.matched.length) {
 		console.log('Route not matched, redirecting to home...')
 		return navigateTo('/launches')
